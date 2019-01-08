7a7bbd017ee477b95553d3ac0d5092462178ef69
Małgorzata Dobrzańska <malgorzata.dobrzanska1@gmail.com>
2019-01-08 21:27:25 +0100

    Header responsive done, section one started

---
+++ js/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    //initialize swiper when document ready
+    var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true
+    });
+    var swiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+
+    var swiper = new Swiper('.swiper-container', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    // var swiper = new Swiper('.swiper-container', {
+    //     // Default parameters
+    //     slidesPerView: 4,
+    //     spaceBetween: 40,
+    //     // Responsive breakpoints
+    //     breakpoints: {
+    //         // when window width is <= 320px
+    //         320: {
+    //             slidesPerView: 1,
+    //             spaceBetween: 10
+    //         },
+    //         // when window width is <= 480px
+    //         480: {
+    //             slidesPerView: 2,
+    //             spaceBetween: 20
+    //         },
+    //         // when window width is <= 640px
+    //         640: {
+    //             slidesPerView: 3,
+    //             spaceBetween: 30
+    //         }
+    //     }
+    // })
+});
