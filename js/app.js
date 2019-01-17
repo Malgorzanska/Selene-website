1c56c3ee60a792d1912bd554f53355486a6c1454
Małgorzata Dobrzańska <malgorzata.dobrzanska1@gmail.com>
2019-01-17 21:45:58 +0100

    new slider

--- js/app.js
+++ js/app.js
@@ -5,13 +5,6 @@ $(document).ready(function () {
         direction: 'horizontal',
         loop: true
     });
-    var swiper = new Swiper('.swiper-container', {
-        pagination: {
-            el: '.swiper-pagination',
-            dynamicMainBullets: 1,
-           // type: '',
-        },
-    });
 
     var swiper = new Swiper('.swiper-container', {
         navigation: {
@@ -25,27 +18,4 @@ $(document).ready(function () {
         spaceBetween: 200
     });
 
-    // var swiper = new Swiper('.swiper-container', {
-    //     // Default parameters
-    //     slidesPerView: 4,
-    //     spaceBetween: 40,
-    //     // Responsive breakpoints
-    //     breakpoints: {
-    //         // when window width is <= 320px
-    //         320: {
-    //             slidesPerView: 1,
-    //             spaceBetween: 10
-    //         },
-    //         // when window width is <= 480px
-    //         480: {
-    //             slidesPerView: 2,
-    //             spaceBetween: 20
-    //         },
-    //         // when window width is <= 640px
-    //         640: {
-    //             slidesPerView: 3,
-    //             spaceBetween: 30
-    //         }
-    //     }
-    // })
 });
